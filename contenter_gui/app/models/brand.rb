184890747777d34c9a5e879f88e78be9018f2071
Kurt Stephens <kurt@trans3.(none)>
2009-07-15 16:38:40 -0500

    Typo in regexp.

--- contenter_gui/app/models/brand.rb
+++ contenter_gui/app/models/brand.rb
@@ -7,7 +7,7 @@
 class Brand < ActiveRecord::Base
   include ContentModel
 
-  validates_format_of :code, :with => /\A([A-Z_][A-Z0-9_]+|_)*\Z/
+  validates_format_of :code, :with => /\A([A-Z_][A-Z0-9_]+|_)\Z/
   validates_uniqueness_of :code
 
 end
