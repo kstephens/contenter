6c4fedfd0eff59c863cad1ae7ca7387b6c9e4ad1
Dean Radcliffe <dradcliffe@dradcliffe-laptop.(none)>
2009-07-31 16:01:07 -0500

    YAMLize in deterministic key order in Plugin::Email
    issue 72216

--- contenter_gui/lib/contenter/plugin/email.rb
+++ contenter_gui/lib/contenter/plugin/email.rb
@@ -25,7 +25,14 @@ module Contenter
       def params_to_data params
         # Ensure keys are Symbols in the YAML.
         params = params.inject({ }){ | h, (k, v) | h[k.to_sym] = v; h }
-        YAML.dump(params)
+        s = <<"END"
+---
+:sender: #{params[:sender]}
+:subject: #{params[:subject]}
+:body: |-
+  #{params[:body].gsub(/(\r?\n)/){|x| x + "  "}}
+END
+        s
       end
 
       
@@ -34,14 +41,14 @@ module Contenter
 
       def edit_view_erb
 <<'END'
+   <tr id="sl_field_content_data_sender">
+     <td class="sl_edit_label"><label for="content_data_sender">Sender</label></td>
+     <td class="sl_edit_value"><input type="text" id="content_data_sender" name="content[data][sender]" width="80" value="<%= obj.data_hash[:sender] %>" /></td></tr>
+
    <tr id="sl_field_content_data_subject">
      <td class="sl_edit_label"><label for="content_data_subject">Subject</label></td>
      <td class="sl_edit_value"><input type="text" id="content_data_subject" name="content[data][subject]" width="80" value="<%= obj.data_hash[:subject] %>"
 /></td></tr>
-
-   <tr id="sl_field_content_data_sender">
-     <td class="sl_edit_label"><label for="content_data_sender">Sender</label></td>
-     <td class="sl_edit_value"><input type="text" id="content_data_sender" name="content[data][sender]" width="80" value="<%= obj.data_hash[:sender] %>" /></td></tr>
 
    <tr id="sl_field_content_data_body">
      <td class="sl_edit_label"><label for="content_data_body">Body</label></td>
