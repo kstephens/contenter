79eabd5848dd60d2851cb4fe44866191a5c765f1
Kurt Stephens <ks.github@kurtstephens.com>
2011-04-01 13:13:57 -0500

    Added migration for role_inheritances table.

---
+++ contenter_gui/db/migrate/20000000000007_create_role_inheritances_table.rb
@@ -0,0 +1,23 @@
+# Create role_inheritances table for inheritable roles.
+# See also 100*4_create_roles.rb.
+class CreateRoleInheritancesTable < ActiveRecord::Migration
+  def self.up
+    if RAILS_ENV != 'development' || ENV['FORCE_MIGRATION']
+      # roles->parent_roles join table
+      tn = :role_inheritances
+      create_table tn do |t|
+        t.integer :child_role_id, :null => false, :references => :roles
+        t.integer :parent_role_id, :null => false, :references => :roles
+        t.integer :sequence, :null => false
+        t.timestamps
+      end
+      add_index tn, :child_role_id
+      add_index tn, :parent_role_id
+      add_index tn, [ :child_role_id, :parent_role_id ], :unique => true
+    end
+  end
+  
+  def self.down
+    drop_table :roles_inheritances
+  end
+end
