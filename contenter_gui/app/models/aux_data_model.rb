ffecf22db51bb26b4ff1f1a7e3fc003992723dcf
Kurt Stephens <ks.github@kurtstephens.com>
2011-04-01 02:25:21 -0500

    Allow access to raw YAML of aux_data.

--- contenter_gui/app/models/aux_data_model.rb
+++ contenter_gui/app/models/aux_data_model.rb
@@ -1,5 +1,7 @@
+require 'yaml'
 
 # Mixin support for arbitrary data Hash column.
+# Also allows access to raw YAML data.
 module AuxDataModel
   def self.included target
     super
@@ -7,7 +9,7 @@ module AuxDataModel
     target.class_eval do 
       include InstanceMethods
 
-      serialize :aux_data # arbitrary data Hash.
+      # serialize :aux_data # arbitrary data Hash.
 
       before_validation :initialize_aux_data!
     end
@@ -17,8 +19,42 @@ module AuxDataModel
   end # module
 
   module InstanceMethods
+    AUX_DATA = 'aux_data'.freeze
+
     def initialize_aux_data!
-      self.aux_data ||= { } 
+      self.aux_data ||= { }
+      self.aux_data_yaml
+    end
+
+    def aux_data_yaml= x
+      # debugger
+      if read_attribute(AUX_DATA) != x
+        write_attribute(AUX_DATA, x)
+        @aux_data_ = false
+        @aux_data = nil
+      end
+    end
+
+    def aux_data_yaml
+      unless @aux_data_yaml_
+        write_attribute(AUX_DATA, YAML::dump(aux_data))
+        @aux_data_yaml_ = true
+      end
+      read_attribute(AUX_DATA)
+    end
+
+    def aux_data= data
+      @aux_data_ = true
+      @aux_data = data
+      @aux_data_yaml_ = false
+    end
+
+    def aux_data
+      unless @aux_data_
+        @aux_data = YAML::load(read_attribute(AUX_DATA))
+        @aux_data_ = true
+      end
+      @aux_data
     end
   end # module
 end
