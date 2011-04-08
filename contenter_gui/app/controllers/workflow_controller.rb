cb78c7d3f74e771abfca8dac7fdf3246d666bc96
Kurt Stephens <ks.github@kurtstephens.com>
2011-04-08 18:50:18 -0500

    Refactor workflow controller to handle each content_type and its plugin separately.

--- contenter_gui/app/controllers/workflow_controller.rb
+++ contenter_gui/app/controllers/workflow_controller.rb
@@ -6,12 +6,6 @@ class WorkflowController < ApplicationController
 
   include CrudController
 
-=begin  
-  # We need to refactor workflow controller to always expect a content_type
-  # so we can call after_object_find.
-  include CnuContenter::Plugin::WorkflowControllerMixin # FIXME! 
-=end
-
   require_capability :ACTION
   before_filter :require_post, :only => [ :perform_status_action ]
 
@@ -59,7 +53,7 @@ class WorkflowController < ApplicationController
     c_ids = content_ids
     cap = ""
 
-    logger.info "Performing action #{status_action.inspect} on content_ids: #{c_ids.inspect}"
+    logger.info { "Performing action #{status_action.inspect} on content_ids: #{c_ids.inspect}" }
     c_id = nil
 
     unless current_user.has_capability?(cap = "controller/<<workflow>>/<<perform_status_action>>/<<#{status_action}>>")
@@ -80,18 +74,46 @@ class WorkflowController < ApplicationController
       content_count = 0
 
       Content.transaction do
+        # Handle each content_type differently.
+        content_types = content_objects.map{|c| c.content_type}.uniq
+        content_types.each do | content_type |
+
+          plugin = content_type.plugin_instance
+          logger.info { "content_type #{content_type} plugin #{plugin}" }
+          
+          # Make sure the content_type's plugin can
+          # support these methods:
+          unless plugin.respond_to?(:plugin_around_perform_status_action)
+            logger.info { "default plugin_around_perform_status_action" }
+            def plugin.plugin_around_perform_status_action *args
+              yield
+            end
+          end
+          
+          unless plugin.respond_to?(:plugin_perform_status_action_capability?)
+            logger.info { "default plugin_around_perform_status_action?" }
+            def plugin.plugin_perform_status_action_capability? *args
+              true
+            end
+          end
+
+          # Filter content by content_type.
+          c_objs = content_objects.select { | c | c.content_type == content_type }
+          logger.info { "Performing action #{status_action.inspect} on #{content_type} content_ids: #{c_objs.map{|c| c.id}.inspect}" }
+        
       # Perform any plugin support around activities in
       # this block.
       # If plugin_around_perform_status_action throws exception,
       # The local transaction should rollback.
-      plugin_around_perform_status_action do 
+      plugin.plugin_around_perform_status_action do 
         VersionList.track_changes_in(@approval_list) do
-          content_objects.each do | c |
+        c_objs.
+          each do | c |
             c_id = c.id
 
             # Check on actual content's type capability.
             if current_user.has_capability?(cap="controller/<<workflow>>/<<perform_status_action>>/<<#{status_action}>>?<<content_type>>=<<#{c.content_type.code}>>") &&
-               plugin_perform_status_action_capability?(c)
+               plugin.plugin_perform_status_action_capability?(c, self)
               c.do_status_action! status_action
               content_count += 1
             else
@@ -103,6 +125,7 @@ class WorkflowController < ApplicationController
           end # c_ids.each
         end   # VersionList.track_changes_in
       end
+        end   # content_types.each
       end     # Content.transaction
 
       flash[:notice] = "#{content_count} Content objects updated."
@@ -111,18 +134,6 @@ class WorkflowController < ApplicationController
     end
 
   end
-
- 
-=begin
-  # Hook for plugins around perform_status_action.
-  def plugin_around_perform_status_action
-    yield
-  end
-
-  def plugin_perform_status_action_capability? content
-    true
-  end
-=end
 
 protected
 
@@ -133,6 +144,7 @@ protected
     @content_objects ||=
       content_ids.map { | i | Content.find(i) }
   end
+  helper_method :content_objects
 
   # Provides the array of content_ids posted from the request.
   # Example: "content"=>{"1"=>{"id"=>"1"}, "2"=>{"id"=>"1"}}
@@ -155,4 +167,12 @@ protected
       (x = params[:status_action]) && x.to_sym
   end
   helper_method :status_action
+
+
+public
+
+  # Support for CnuContenter::Plugin::WorkflowControllerMixin
+  public :current_user
+  public :status_action
+
 end
